3e86f109473cc85be75af1ef2cf65e73fee8b046
Seth Hall <1033332+sethburtonhall@users.noreply.github.com>
2022-05-17 10:15:57 -0400

    customizes function event body

--- serverless-functions/netlify/functions/pokedex.js
+++ serverless-functions/netlify/functions/pokedex.js
@@ -1,7 +1,10 @@
 import fetch from 'node-fetch';
 
-exports.handler = async function () {
-  const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto';
+exports.handler = async function (event, context) {
+  const eventBody = JSON.parse(event.body);
+  const POKE_API = `https://pokeapi.co/api/v2/pokedex/${eventBody.region}`;
+
+  console.log(eventBody);
 
   const response = await fetch(POKE_API);
   const data = await response.json();
